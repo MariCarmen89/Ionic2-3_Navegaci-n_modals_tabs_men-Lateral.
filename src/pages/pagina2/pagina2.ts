4d253744b8a710f9c928c5f16de9d61a05235981
MariCarmen89 <maricarmenbaena@ingenieros.com>
2017-07-04 20:52:59 +0200

    navCtrl -> pop & popToRoot

---
+++ src/pages/pagina2/pagina2.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Pagina3Page } from "../index.paginas";
+
+@Component({
+  selector: 'page-pagina2',
+  templateUrl: 'pagina2.html',
+})
+export class Pagina2Page {
+  pagina3:any = Pagina3Page;
+  
+  mutantes:any[] = [{
+    nombre: "Magneto",
+    poder: "Controlar metales"
+  },
+  {
+    nombre: "Wolverine",
+    poder: "Regeneración acelerada"
+  },
+  {
+    nombre: "Profesor X",
+    poder: "Poderes psiquicos"
+  },
+  {
+    nombre: "Gambito",
+    poder: "Cargar objetos animados con energia"
+  }];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  irPagina3( mutante: any ){
+    console.log( mutante );
+    this.navCtrl.push( Pagina3Page, { 'personaje': mutante } );
+  }
+}
