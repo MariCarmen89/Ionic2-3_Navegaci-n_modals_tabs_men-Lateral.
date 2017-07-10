40a5fb8f940dc3a09a95cc269dd31c6bb184d23e
MariCarmen89 <maricarmenbaena@ingenieros.com>
2017-07-10 18:39:50 +0200

    menuToggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/index.paginas';
+import { TabsPage, Ajustes2Page } from '../pages/index.paginas';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  tabs:any = TabsPage;
+  ajustes2 = Ajustes2Page;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage:any = TabsPage;
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private menuCtrl: MenuController) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  abrirPagina( pagina: any) {
+    this.rootPage = pagina;
+    this.menuCtrl.close();
   }
 }
